33c79fd18763578a777462c90aa1c05b50de5108
vorgoron <exey13@gmail.com>
2015-04-20 23:38:10 +0400

    раскрашены спиннер выбора цвета, начало внедрения app list

--- app/src/main/java/ru/udmspell/datewidget/ConfigActivity.java
+++ app/src/main/java/ru/udmspell/datewidget/ConfigActivity.java
@@ -2,13 +2,25 @@ package ru.udmspell.datewidget;
 
 import android.app.Activity;
 import android.appwidget.AppWidgetManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+
+import ru.udmspell.datewidget.adapters.ColorsSpinnerAdapter;
 
 public class ConfigActivity extends Activity {
 
@@ -30,6 +42,7 @@ public class ConfigActivity extends Activity {
     public final static String WIDGET_PREF = "widget_pref";
     public final static String TEXT_COLOR = "text_color_";
     private Spinner textColorSpinner;
+    private Spinner appListSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,50 +71,24 @@ public class ConfigActivity extends Activity {
         setContentView(R.layout.config_layout);
 
         textColorSpinner = (Spinner) findViewById(R.id.text_color_spinner);
-        textColorSpinner.setAdapter(new ArrayAdapter<String>(this, R.layout.spiner_item, R.id.text,
+        textColorSpinner.setAdapter(new ColorsSpinnerAdapter(this, R.layout.color_spinner_item, R.id.text,
                 getResources().getStringArray(R.array.color_names)));
+
+        appListSpinner = (Spinner) findViewById(R.id.appsSpinner);
+        ArrayList<AppInfo> appInfoArrayList = getListOfInstalledApp(this);
+        appListSpinner.setAdapter(new ArrayAdapter<>(this, R.layout.app_spinner_item,
+                R.id.appName, appInfoArrayList));
     }
 
 
     public void onClick(View v) {
 
         int selTextColorPos = textColorSpinner.getSelectedItemPosition();
-        int textColor;
-        switch (selTextColorPos) {
-            case WHITE:
-                textColor = getResources().getColor(R.color.white);
-                break;
-            case BLACK:
-                textColor = getResources().getColor(R.color.black);
-                break;
-            case RED:
-                textColor = getResources().getColor(R.color.red);
-                break;
-            case PINK:
-                textColor = getResources().getColor(R.color.pink);
-                break;
-            case BLUE:
-                textColor = getResources().getColor(R.color.blue);
-                break;
-            case GREEN:
-                textColor = getResources().getColor(R.color.green);
-                break;
-            case YELLOW:
-                textColor = getResources().getColor(R.color.yellow);
-                break;
-            case PURPLE:
-                textColor = getResources().getColor(R.color.purple);
-                break;
-            case GREY:
-                textColor = getResources().getColor(R.color.grey);
-                break;
-            default:
-                textColor = getResources().getColor(R.color.white);
-        }
+        int textColor = getColor(selTextColorPos);
 
         // Записываем значения с экрана в Preferences
         SharedPreferences sp = getSharedPreferences(WIDGET_PREF, MODE_PRIVATE);
-        sp.edit().putInt(TEXT_COLOR + widgetID, textColor).apply();
+        sp.edit().putInt(TEXT_COLOR + widgetID, getResources().getColor(textColor)).apply();
 
         // положительный ответ
         setResult(RESULT_OK, resultValue);
@@ -109,10 +96,88 @@ public class ConfigActivity extends Activity {
         Intent intent = new Intent(this, DateWidget.class);
         intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
         int[] ids = {widgetID};
-        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS,ids);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
         sendBroadcast(intent);
 
         Log.d(LOG_TAG, "finish config " + widgetID);
         finish();
+    }
+
+    public static int getColor(int position) {
+        int textColor;
+        switch (position) {
+            case WHITE:
+                textColor = R.color.white;
+                break;
+            case BLACK:
+                textColor = R.color.black;
+                break;
+            case RED:
+                textColor = R.color.red;
+                break;
+            case PINK:
+                textColor = R.color.pink;
+                break;
+            case BLUE:
+                textColor = R.color.blue;
+                break;
+            case GREEN:
+                textColor = R.color.green;
+                break;
+            case YELLOW:
+                textColor = R.color.yellow;
+                break;
+            case PURPLE:
+                textColor = R.color.purple;
+                break;
+            case GREY:
+                textColor = R.color.grey;
+                break;
+            default:
+                textColor = R.color.white;
+        }
+        return textColor;
+    }
+
+    private static ArrayList getListOfInstalledApp(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        List apps = packageManager.getInstalledPackages(PackageManager.SIGNATURE_MATCH);
+        if (apps != null && !apps.isEmpty()) {
+            ArrayList installedApps = new ArrayList();
+            for (int i = 0; i < apps.size(); i++) {
+                PackageInfo p = (PackageInfo) apps.get(i);
+                ApplicationInfo appInfo = null;
+                try {
+                    appInfo = packageManager.getApplicationInfo(p.packageName, 0);
+                    AppInfo app = new AppInfo();
+                    app.setName(p.applicationInfo.loadLabel(packageManager).toString());
+                    app.setPackageName(p.packageName);
+                    app.setVersionName(p.versionName);
+                    app.setVersionCode(p.versionCode);
+                    app.setIcon(p.applicationInfo.loadIcon(packageManager));
+
+                    //check if the application is not an application system
+                    Intent launchIntent = app.getLaunchIntent(context);
+                    if(launchIntent != null && (appInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0)
+                        installedApps.add(app);
+                } catch (PackageManager.NameNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            //sort the list of applications alphabetically
+            if (installedApps.size() > 0) {
+                Collections.sort(installedApps, new Comparator<AppInfo>() {
+
+                    @Override
+                    public int compare(final AppInfo app1, final AppInfo app2) {
+                        return app1.getName().toLowerCase(Locale.getDefault()).compareTo(app2.getName().toLowerCase(Locale.getDefault()));
+                    }
+
+                });
+            }
+            return installedApps;
+        }
+        return null;
     }
 }
